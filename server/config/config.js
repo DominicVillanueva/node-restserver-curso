d5c3212450c1474a8eeeed927d11f6f7a8e71245
DominicVillanueva <partner.dominic@hotmail.com>
2021-02-05 01:28:30 -0500

    Variable de entorno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -11,5 +11,5 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'DEV';
 // =============
 // Base de datos
 // =============
-let urlDB = process.env.NODE_ENV === 'DEV' ? 'mongodb://localhost:27017/cafe' : 'mongodb+srv://dominicavs:SftGThHmoDoW0Qo5@cluster0.gouvg.mongodb.net/cafe';
+let urlDB = process.env.NODE_ENV === 'DEV' ? 'mongodb://localhost:27017/cafe' : process.env.MONGODB_URI;
 process.env.URL_DB = urlDB;
